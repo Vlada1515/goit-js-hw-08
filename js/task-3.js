d7648707cc6c727f090395e39f646bc7b8497bbd
Vlada1515 <vladavalianova5@gmail.com>
2024-03-03 11:19:25 +0100

    done

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector("#name-input");
-const output = document.querySelector("#name-output");
-const listenerInput = textInput.addEventListener("input", (event) => {
-  const timedInput = event.target.value.trim();
-  output.textContent = timedInput || "Anonymous";
-});
